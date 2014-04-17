600a0d0b0b80110345eb72052a1a7cdf7f6e73db
ozgur <cozgur1907@gmail.com>
2014-04-17 00:26:22 +0000

    added css, js, includes folders

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header>
+    	<h1>Welcome to Widgco</h1>
+        <nav>
+        	<a href="">Home</a>
+            <a href="">About</a>
+			<a href="">Contact</a>
+        </nav>
+</header>
--- index.php
+++ index.php
@@ -8,18 +8,11 @@
 
 <body>
 
-	<header>
-    	<h1>Welcome to Widgco</h1>
-        <nav>
-        	<a href="">Home</a>
-            <a href="">About</a>
-			<a href="">Contact</a>
-        </nav>
-     </header>
+  <?php require_once('includes/header.php')?>
      <section>
      	<h2>Intro</h2>
      	<p>
-        <img src="ceo.jpg" alt="my pic" width="100px" height="100px" />
+        <img src="images/ceo.jpg" alt="my pic" width="100px" height="100px" />
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         
         </p>
@@ -27,9 +20,7 @@
      
      </section>
      
-     <footer>
-     	<p>Widgco Widgets Inc.&trade; &copy;2014</p>
-
+  <?php require_once('includes/footer.php')?>
 
 </body>
 </html>
